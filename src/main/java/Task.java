bb1e4cb91214d7abab38e9760ebc53236521fb64
rishi <rishi5154@gmail.com>
2020-08-14 20:25:08 +0800

    Level3

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -8,8 +8,12 @@ public class Task {
         this.isDone = false;
         this.index = index;
     }
+    public String getStatusWithIndex() {
+        return isDone ? index + ". " + "[\u2713] " + this.description
+                : index + ". " + "[\u2718] " + this.description; //return tick or X symbols
+    }
 
     public String toString() {
-        return this.index + ". " + this.description;
+        return (isDone ? "[\u2713] " + this.description : "[\u2718] " + this.description);
     }
 }
